cbcb469353e3be32b7219e8e9fe05ed0feaf1727
anurag <anuragkumar123@gmail.com>
2024-07-03 17:25:00 +0000

    added new

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ public class Demo{
 	public static void main(String args[]){
 		//print statement fot hello world
 		System.out.println("Hello World");
-		//print statement fot devops
+		//print statement fot devops for new
 		system.out.println("welcome to DevOps");
 
 
