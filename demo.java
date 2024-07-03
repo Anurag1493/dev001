ab919c831dff7a2d7b15ec9d137d2489366be059
anurag <anuragkumar123@gmail.com>
2024-07-03 06:21:12 +0000

    Devops changes

--- demo.java
+++ demo.java
@@ -2,6 +2,10 @@ public class Demo{
 	public static void main(String args[]){
 		//print statement fot hello world
 		System.out.println("Hello World");
+		//print statement fot devops
+		system.out.println("welcome to DevOps");
+
+
 	}
 }
 
